ae6e533365a2dfcf0bf7596c9382f84937225c4b
cupcakearmy <hi@nicco.io>
2025-10-27 18:28:41 +0100

    add 1k5

---
+++ hosts/1k5.nix
@@ -0,0 +1,15 @@
+{
+  username = "nicco";
+  hostName = "1k5";
+  platform = "aarch64-darwin";
+  sshKey = "1k5";
+
+  extras = {
+    casks = [
+      "notion"
+    ];
+    pkgs =
+      pkgs: with pkgs; [
+      ];
+  };
+}
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,4 +1,5 @@
 [
   (import ./mac14.nix)
   (import ./ordio.nix)
+  (import ./1k5.nix)
 ]
